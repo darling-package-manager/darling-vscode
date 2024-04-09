354c4543328a567d98e1cefbb658a538e7031127
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-08 20:52:02 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use darling_api as darling;
+
+pub static PACKAGE_MANAGER: VSCode = VSCode;
+
+pub struct VSCode;
+
+impl darling::PackageManager for VSCode {
+    fn name(&self) -> String {
+        "vscode".to_owned()
+    }
+
+    fn install(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<Option<String>> {
+        std::process::Command::new(code_or_codium()?)
+            .arg("--install-extension")
+            .arg(&package.name)
+            .spawn()?
+            .wait()?;
+
+        let version_pattern = regex::Regex::new(&format!("{}@(.+)", package.name))?;
+        let extension_info = String::from_utf8(
+            std::process::Command::new(code_or_codium()?)
+                .arg("--show-versions")
+                .arg("--list-extensions")
+                .output()?
+                .stdout,
+        )?;
+
+        Ok(Some(
+            version_pattern
+                .captures(&extension_info)
+                .ok_or_else(|| anyhow::anyhow!("Erorr parsing VSCode extension data"))?[1]
+                .to_owned(),
+        ))
+    }
+
+    fn uninstall(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
+        std::process::Command::new(code_or_codium()?)
+            .arg("--uninstall-extension")
+            .arg(&package.name)
+            .spawn()?
+            .wait()?;
+        Ok(())
+    }
+
+    fn get_all_explicit(&self, _context: &darling::Context) -> anyhow::Result<Vec<(String, String)>> {
+        Ok(String::from_utf8(
+            std::process::Command::new(code_or_codium()?)
+                .arg("--show-versions")
+                .arg("--list-extensions")
+                .output()?
+                .stdout,
+        )?
+        .split('\n')
+        .map(|line| {
+            let parts = line.split('@').collect::<Vec<_>>();
+            (parts[0].to_owned(), parts[1].to_owned())
+        })
+        .collect::<Vec<_>>())
+    }
+}
+
+fn code_or_codium() -> anyhow::Result<&'static str> {
+    if which::which("code").is_ok() {
+        return Ok("code");
+    }
+
+    if which::which("codium").is_ok() {
+        return Ok("codium");
+    }
+
+    Err(anyhow::anyhow!("No installation of VSCode or VSCodium found!"))
+}
